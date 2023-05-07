76519e46e16289f6d6a9d2fcb1f4d33c48a470b9
Maciess <m4ciess@gmail.com>
2023-05-07 20:21:51 +0200

    Add file to generate data

--- motif_315688_generate.py
+++ motif_315688_generate.py
@@ -4,6 +4,8 @@ import numpy as np
  
 import argparse 
 
+gen_dict = { 1: 'A', 2: 'C', 3: 'G', 4: 'T'}
+NUM_OF_GENS = 4
  
 # Musimy wczytać parametry
 
@@ -21,6 +23,7 @@ param_file, output_file = ParseArguments()
 with open(param_file, 'r') as inputfile:
     params = json.load(inputfile)
  
+
  
 w=params['w']
 k=params['k']
@@ -29,22 +32,33 @@ Theta = np.asarray(params['Theta'])
 ThetaB =np.asarray(params['ThetaB'])
 
 
-# TO DO: wywymulowac x_1, ..., x_k, gdzie x_i=(x_{i1},..., x_{iw}) 
-# zgodnie z opisem jak w skrypcie i zapisac w pliku .csv
-# i-ta linijka = x_i
+def generate_experiment_matrix(num_of_obserwation: int = k, length_of_motif : int = w):
+    return np.random.uniform(size = (num_of_obserwation, length_of_motif))
 
-# Dla przykladu, zalozmy, ze x_1,... x_k sa zebrane w macierz X
-# (dla przypomnienia: kazdy x_{ij} to A,C,G lub T, co utozsamiamy z 1,2,3,4
+def convert_prob_into_value(value, prog_vector): #da sie pewnie ładniej jakis refaktor poxniej
+    gen = NUM_OF_GENS + 1
+    for prog in reversed(prog_vector):
+        if prog < value:
+            break
+        gen = gen - 1
+    return gen
 
-X=np.random.randint(4, size=(k,w))+1
 
-# Musimy zapisac powyzszy X oraz alpha (k i w mozna potem odczytac z X)
+X = generate_experiment_matrix()
+
+Z = np.random.binomial(n = 1, p = alpha, size = k)
+
+for row_index, col_index in np.ndindex(X.shape):
+    if Z[row_index] == 1:
+        prob_vector = np.cumsum(Theta[:, col_index])
+    else:
+        prob_vector = np.cumsum(ThetaB)
+    X[row_index, col_index] = convert_prob_into_value(X[row_index, col_index], prob_vector)
 
 gen_data = {    
     "alpha" : alpha,
     "X" : X.tolist()
     }
-
 
 
 with open(output_file, 'w') as outfile:
