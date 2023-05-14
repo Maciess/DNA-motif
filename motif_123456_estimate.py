7c72a876671397fa12f71fdb5d74456bae5c7201
Maciess <m4ciess@gmail.com>
2023-05-14 22:32:41 +0200

    Add implementation of EM algorithm

--- motif_123456_estimate.py
+++ motif_123456_estimate.py
@@ -1,7 +1,5 @@
 import json
-
 import numpy as np
-
 import argparse
 from motif_315688_generate import NUM_OF_GENS
 
@@ -24,17 +22,34 @@ with open(input_file, 'r') as inputfile:
     data = json.load(inputfile)
 
 alpha = data['alpha']
-X = np.asarray(data['X'])
-k, w = X.shape
-
+X = np.asarray(data['X'], dtype=int) - 1  # for easier index mapping
+NUM_SAMPLE, LEN_MOTIF = X.shape
+MAX_ITER = 200
 # Sample from dirichlet distribution sum up to 1 and that's give us nice oneliner.
-ThetaB = np.random.dirichlet(np.ones(NUM_OF_GENS), size=1)[0]
-Theta = np.random.dirichlet(np.ones(NUM_OF_GENS), size=w).transpose()
+# thetaB = np.random.dirichlet(np.ones(NUM_OF_GENS), size=1)[0]
+# theta = np.random.dirichlet(np.ones(NUM_OF_GENS), size=LEN_MOTIF).transpose()
+thetaB = np.full(shape=NUM_OF_GENS, fill_value=1 / NUM_OF_GENS, dtype=np.float64)
+theta = np.full(shape=(NUM_OF_GENS, LEN_MOTIF), fill_value=1 / NUM_OF_GENS, dtype=np.float64)
+
+for _ in range(MAX_ITER):
+    print(_)
+    Q_0 = (1 - alpha) * np.prod(thetaB[X], axis=1)
+    lambda_Q_0 = LEN_MOTIF * np.sum(Q_0)
+
+    Q_1 = alpha * np.prod(theta[X, np.arange(LEN_MOTIF)], axis=1)
+    lambda_Q_1 = np.sum(Q_1)
+
+    for row_index in range(NUM_OF_GENS):
+        X_ind = X == row_index
+        thetaB[row_index] = np.sum(Q_0 * np.sum(X_ind, axis=1)) / lambda_Q_0
+        for col_index in range(LEN_MOTIF):
+            theta[row_index, col_index] = np.sum(Q_1[X_ind[:, col_index]]) / lambda_Q_1
+    print((_, thetaB))
 
 estimated_params = {
     "alpha": alpha,
-    "Theta": Theta.tolist(),
-    "ThetaB": ThetaB.tolist()
+    "Theta": theta.tolist(),
+    "ThetaB": thetaB.tolist()
 }
 
 with open(output_file, 'w') as outfile:
--- motif_315688_generate.py
+++ motif_315688_generate.py
@@ -17,35 +17,36 @@ def ParseArguments():
 
 param_file, output_file = ParseArguments()
 
-with open(param_file, 'r') as inputfile:
-    params = json.load(inputfile)
+if __name__ == "__main__":
+    with open(param_file, 'r') as inputfile:
+        params = json.load(inputfile)
 
-w = params['w']
-k = params['k']
-alpha = params['alpha']
-Theta = np.asarray(params['Theta'])
-ThetaB = np.asarray(params['ThetaB'])
-
-
-def generate_experiment_matrix(num_of_observation: int = k, length_of_motif: int = w):
-    return np.empty(shape=(num_of_observation, length_of_motif), dtype=np.float64)
+    w = params['w']
+    k = params['k']
+    alpha = params['alpha']
+    Theta = np.asarray(params['Theta'])
+    ThetaB = np.asarray(params['ThetaB'])
 
 
-X = generate_experiment_matrix()
+    def generate_experiment_matrix(num_of_observation: int = k, length_of_motif: int = w):
+        return np.empty(shape=(num_of_observation, length_of_motif), dtype=np.float64)
 
-Z = np.random.binomial(n=1, p=alpha, size=k)
 
-for row_index, col_index in np.ndindex(X.shape):
-    if Z[row_index] == 1:
-        prob_vector = Theta[:, col_index]
-    else:
-        prob_vector = ThetaB
-    X[row_index, col_index] = np.random.choice(NUM_OF_GENS, size=1, p=prob_vector) + 1
+    X = generate_experiment_matrix()
 
-gen_data = {
-    "alpha": alpha,
-    "X": X.tolist()
-}
+    Z = np.random.binomial(n=1, p=alpha, size=k)
 
-with open(output_file, 'w') as outfile:
-    json.dump(gen_data, outfile)
+    for row_index, col_index in np.ndindex(X.shape):
+        if Z[row_index] == 1:
+            prob_vector = Theta[:, col_index]
+        else:
+            prob_vector = ThetaB
+        X[row_index, col_index] = np.random.choice(NUM_OF_GENS, size=1, p=prob_vector) + 1
+
+    gen_data = {
+        "alpha": alpha,
+        "X": X.tolist()
+    }
+
+    with open(output_file, 'w') as outfile:
+        json.dump(gen_data, outfile)
