b6d06dd1c5c953f60e7560650994c6e1e102b7a8
Maciess <m4ciess@gmail.com>
2023-05-20 19:38:00 +0200

    Fix bug. Normalize Q's.

--- motif_123456_estimate.py
+++ motif_123456_estimate.py
@@ -34,13 +34,19 @@ theta = np.full(shape=(NUM_OF_GENS, LEN_MOTIF), fill_value=1 / NUM_OF_GENS, dtyp
 for _ in range(MAX_ITER):
     print(_)
     Q_0 = (1 - alpha) * np.prod(thetaB[X], axis=1)
-    lambda_Q_0 = LEN_MOTIF * np.sum(Q_0)
-
     Q_1 = alpha * np.prod(theta[X, np.arange(LEN_MOTIF)], axis=1)
+
+    common_denominator = Q_0 + Q_1
+
+    Q_0 = Q_0 / common_denominator
+    Q_1 = Q_1 / common_denominator
+
+    lambda_Q_0 = LEN_MOTIF * np.sum(Q_0)
     lambda_Q_1 = np.sum(Q_1)
 
     for row_index in range(NUM_OF_GENS):
         X_ind = X == row_index
+        c = np.sum(X_ind, axis=1)
         thetaB[row_index] = np.sum(Q_0 * np.sum(X_ind, axis=1)) / lambda_Q_0
         for col_index in range(LEN_MOTIF):
             theta[row_index, col_index] = np.sum(Q_1[X_ind[:, col_index]]) / lambda_Q_1
