c04ef9cca11c6243cb60d13d45d67ada81060373
Maciess <m4ciess@gmail.com>
2023-05-25 10:51:09 +0200

    Add test script

---
+++ motif_315688_estimate.py
@@ -0,0 +1,68 @@
+import json
+import numpy as np
+import argparse
+from motif_315688_generate import NUM_OF_GENS
+
+
+def ParseArguments():
+    parser = argparse.ArgumentParser(description="Motif generator")
+    parser.add_argument('--input', default="generated_data.json", required=False,
+                        help='Plik z danymi  (default: %(default)s)')
+    parser.add_argument('--output', default="estimated_params.json", required=False,
+                        help='Tutaj zapiszemy wyestymowane parametry  (default: %(default)s)')
+    parser.add_argument('--estimate-alpha', default="no", required=False,
+                        help='Czy estymowac alpha czy nie?  (default: %(default)s)')
+    args = parser.parse_args()
+    return args.input, args.output, args.estimate_alpha
+
+
+input_file, output_file, estimate_alpha = ParseArguments()
+
+with open(input_file, 'r') as inputfile:
+    experiment_data = json.load(inputfile)
+
+alpha = experiment_data['alpha']
+X = np.asarray(experiment_data['X'], dtype=int) - 1  # for easier index mapping
+NUM_SAMPLE, LEN_MOTIF = X.shape
+MAX_ITER = 200
+# Sample from dirichlet distribution sum up to 1 and that's give us nice oneliner.
+
+initial_thetaB = np.full(shape=NUM_OF_GENS, fill_value=1 / NUM_OF_GENS, dtype=np.float64)
+initial_theta = np.full(shape=(NUM_OF_GENS, LEN_MOTIF), fill_value=1 / NUM_OF_GENS, dtype=np.float64)
+
+
+def EM(data=X, theta=initial_theta, thetaB=initial_thetaB):
+    for _ in range(MAX_ITER):
+        Q_0 = (1 - alpha) * np.prod(thetaB[data], axis=1)
+        Q_1 = alpha * np.prod(theta[data, np.arange(LEN_MOTIF)], axis=1)
+
+
+        #likelyhood_function = np.sum(Q_0 * np.log(Q_0) + Q_1 * np.log(Q_1))
+
+        common_denominator = Q_0 + Q_1
+        Q_0 = Q_0 / common_denominator
+        Q_1 = Q_1 / common_denominator
+
+        lambda_Q_0 = LEN_MOTIF * np.sum(Q_0)
+        lambda_Q_1 = np.sum(Q_1)
+
+        for row_index in range(NUM_OF_GENS):
+            X_ind = data == row_index
+            thetaB[row_index] = np.sum(Q_0 * np.sum(X_ind, axis=1)) / lambda_Q_0
+            for col_index in range(LEN_MOTIF):
+                theta[row_index, col_index] = np.sum(Q_1[X_ind[:, col_index]]) / lambda_Q_1
+
+    return theta, thetaB
+
+
+if __name__ == "__main__":
+    result_theta, result_theta_b = EM()
+
+    estimated_params = {
+        "alpha": alpha,
+        "Theta": result_theta.tolist(),
+        "ThetaB": result_theta_b.tolist()
+    }
+
+    with open(output_file, 'w') as outfile:
+        json.dump(estimated_params, outfile)
---
+++ test_script.py
@@ -0,0 +1,62 @@
+import numpy as np
+import json
+from motif_315688_estimate import X, EM, NUM_OF_GENS, LEN_MOTIF
+
+if __name__ == "__main__":
+    with open('params_set1.json', 'r') as inputfile:
+        params = json.load(inputfile)
+    theta_orig = np.asarray(params['Theta'])
+    theta_b_orig = np.asarray(params['ThetaB'])
+
+
+def d_tv(prob_orig, prob_estim):
+    return 0.5 * np.sum(np.abs(prob_orig - prob_estim))
+
+
+## random
+print('random')
+initial_theta = np.random.dirichlet(np.ones(NUM_OF_GENS), size=LEN_MOTIF).transpose()
+initial_theta_b = np.random.dirichlet(np.ones(NUM_OF_GENS), size=1)[0]
+
+estim_theta, estim_theta_b = EM(data=X, theta=initial_theta, thetaB=initial_theta_b)
+
+total_distance = 1 / (LEN_MOTIF + 1) * (
+        d_tv(prob_orig=theta_b_orig, prob_estim=estim_theta_b) + d_tv(prob_orig=theta_orig.flatten(),
+                                                                      prob_estim=estim_theta.flatten()))
+
+print(total_distance)
+
+print('uniform')
+
+initial_thetaB = np.full(shape=NUM_OF_GENS, fill_value=1 / NUM_OF_GENS, dtype=np.float64)
+initial_theta = np.full(shape=(NUM_OF_GENS, LEN_MOTIF), fill_value=1 / NUM_OF_GENS, dtype=np.float64)
+
+estim_theta, estim_theta_b = EM(data=X, theta=initial_theta, thetaB=initial_theta_b)
+
+total_distance = 1 / (LEN_MOTIF + 1) * (
+        d_tv(prob_orig=theta_b_orig, prob_estim=estim_theta_b) + d_tv(prob_orig=theta_orig.flatten(),
+                                                                      prob_estim=estim_theta.flatten()))
+print(total_distance)
+
+print('bootstrap')
+
+
+thetas = []
+thetas_b = []
+for _ in range(20):
+    initial_theta = np.random.dirichlet(np.ones(NUM_OF_GENS), size=LEN_MOTIF).transpose()
+    initial_theta_b = np.random.dirichlet(np.ones(NUM_OF_GENS), size=1)[0]
+
+    estim_theta, estim_theta_b = EM(data=X, theta=initial_theta, thetaB=initial_theta_b)
+    thetas.append(estim_theta)
+    thetas_b.append(estim_theta_b)
+
+stacked_thetas = np.stack(thetas, axis=0)
+stacked_thetas_b = np.stack(thetas_b, axis=0)
+estim_theta = np.mean(stacked_thetas, axis=0)
+estim_theta_b = np.mean(stacked_thetas_b, axis=0)
+
+total_distance = 1 / (LEN_MOTIF + 1) * (
+        d_tv(prob_orig=theta_b_orig, prob_estim=estim_theta_b) + d_tv(prob_orig=theta_orig.flatten(),
+                                                                      prob_estim=estim_theta.flatten()))
+print(total_distance)
