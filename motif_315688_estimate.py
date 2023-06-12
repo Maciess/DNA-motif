c36db8b626e848f67a74af01e5372a5fe4aa78a1
Maciess <m4ciess@gmail.com>
2023-06-12 22:41:25 +0200

    fix issue with argparse

--- motif_315688_estimate.py
+++ motif_315688_estimate.py
@@ -1,7 +1,8 @@
 import json
 import numpy as np
 import argparse
-from motif_315688_generate import NUM_OF_GENS
+
+NUM_OF_GENS = 4
 
 
 def ParseArguments():
@@ -10,8 +11,8 @@ def ParseArguments():
                         help='Plik z danymi  (default: %(default)s)')
     parser.add_argument('--output', default="estimated_params.json", required=False,
                         help='Tutaj zapiszemy wyestymowane parametry  (default: %(default)s)')
-    parser.add_argument('--estimate-alpha', default="no", required=False,
-                        help='Czy estymowac alpha czy nie?  (default: %(default)s)')
+    parser.add_argument('--estimate-alpha', action='store_true',
+                        help='Czy estymowac alpha czy nie? (default: %(default)s)')
     args = parser.parse_args()
     return args.input, args.output, args.estimate_alpha
 
@@ -25,7 +26,6 @@ alpha = experiment_data['alpha']
 X = np.asarray(experiment_data['X'], dtype=int) - 1  # for easier index mapping
 NUM_SAMPLE, LEN_MOTIF = X.shape
 MAX_ITER = 200
-# Sample from dirichlet distribution sum up to 1 and that's give us nice oneliner.
 
 initial_thetaB = np.full(shape=NUM_OF_GENS, fill_value=1 / NUM_OF_GENS, dtype=np.float64)
 initial_theta = np.full(shape=(NUM_OF_GENS, LEN_MOTIF), fill_value=1 / NUM_OF_GENS, dtype=np.float64)
@@ -44,13 +44,12 @@ def EM(data=X, theta=initial_theta, thetaB=initial_thetaB, estimate_alpha=False,
         Q_1 = Q_1 / common_denominator
 
         likelyhood_function[_] = np.sum(Q_0 * np.log(Q_0) + Q_1 * np.log(Q_1))
-        print(likelyhood_function[_])
-        if _ > 0 and np.abs(likelyhood_function[_] - likelyhood_function[_ - 1]) < np.abs(
+
+        if _ > 20 and np.abs(likelyhood_function[_] - likelyhood_function[_ - 1]) < np.abs(
                 tol * likelyhood_function[_ - 1]):
-            print("COVERAGE")
             break
 
-        alpha = np.sum(Q_1) / (np.sum(Q_1) + np.sum(Q_0)) if estimate_alpha else alpha
+        alpha = np.sum(Q_1) / np.sum(Q_1 + Q_0) if estimate_alpha else alpha
 
         lambda_Q_0 = LEN_MOTIF * np.sum(Q_0)
         lambda_Q_1 = np.sum(Q_1)
@@ -61,16 +60,39 @@ def EM(data=X, theta=initial_theta, thetaB=initial_thetaB, estimate_alpha=False,
             for col_index in range(LEN_MOTIF):
                 theta[row_index, col_index] = np.sum(Q_1[X_ind[:, col_index]]) / lambda_Q_1
 
-    return theta, thetaB, likelyhood_function
+    return theta, thetaB, alpha, likelyhood_function
 
+
+B = 25
 
 if __name__ == "__main__":
-    result_theta, result_theta_b, _ = EM()
+
+    thetas_b = []
+    thetas = []
+    alpha_vec = []
+
+    for _ in range(B):
+        initial_theta = np.random.dirichlet(np.ones(NUM_OF_GENS), size=LEN_MOTIF).transpose()
+        initial_theta_b = np.random.dirichlet(np.ones(NUM_OF_GENS), size=1)[0]
+        estim_theta, estim_theta_b, estim_alpha, loglik = EM(data=X, theta=initial_theta,
+                                                             thetaB=initial_theta_b,
+                                                             estimate_alpha=estimate_alpha)
+
+        thetas.append(estim_theta)
+        thetas_b.append(estim_theta_b)
+        alpha_vec.append(estim_alpha)
+
+    stacked_thetas = np.stack(thetas, axis=0)
+    stacked_thetas_b = np.stack(thetas_b, axis=0)
+
+    estim_theta = np.mean(stacked_thetas, axis=0)
+    estim_theta_b = np.mean(stacked_thetas_b, axis=0)
+    estim_alpha = np.mean(np.array(estim_alpha))
 
     estimated_params = {
-        "alpha": alpha,
-        "Theta": result_theta.tolist(),
-        "ThetaB": result_theta_b.tolist()
+        "alpha": estim_alpha,
+        "Theta": estim_theta.tolist(),
+        "ThetaB": estim_theta_b.tolist()
     }
 
     with open(output_file, 'w') as outfile:
